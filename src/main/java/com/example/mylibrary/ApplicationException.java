993ab2e57054fcc52a36c8daf20dbd9f305fa969
Jaroslaw Kowalczyk <jar.kowalczyk87@gmail.com>
2017-02-17 14:02:34 +0100

    method of the superclass used instead of overriding in exception class

--- src/main/java/com/example/mylibrary/ApplicationException.java
+++ src/main/java/com/example/mylibrary/ApplicationException.java
@@ -4,12 +4,9 @@ package com.example.mylibrary;
  * Created by a591566 on 2017-02-17.
  */
 public class ApplicationException extends RuntimeException{
-    private String message;
+
     public ApplicationException(String message){
-        this.message = message;
+        super(message);
     }
 
-    public String getMessage(){
-        return this.message;
-    }
 }
